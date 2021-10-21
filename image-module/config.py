360909bf4b2fac750421bbdff06cf7174fa1b275
denis2111 <denis.banu99@yahoo.com>
2021-10-21 21:47:03 +0300

    Add basic files for image-module.

---
+++ image-module/config.py
@@ -0,0 +1,6 @@
+import os
+
+DATASET_PATH = "dataset"
+
+# path where the model will be saved
+MODEL_PATH = os.path.sep.join(["model", "natural_disaster.model"])
--- image-module/main.py
+++ image-module/main.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from train import train
+from predict import predict
+import tensorflow as tf
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return 'Hello World - this is the image module'
+
+@app.route('/classify_raw')
+def classify_raw(raw_image):
+	predict()
+	return 'classify_raw'
+
+@app.route('/classify')
+def classify(image):
+	predict()
+	return 'classify'
+
+@app.route('/train')
+def train():
+	train()
+	return 'training'
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", debug=True)
