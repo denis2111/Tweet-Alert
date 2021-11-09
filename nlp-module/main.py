46f84ec9ddec2a18805d38eacb48561d0c4707d8
AndreiAvram99 <53441456+AndreiAvram99@users.noreply.github.com>
2021-11-10 01:48:11 +0200

    NLP finish translator + start tokenizer

--- nlp-module/main.py
+++ nlp-module/main.py
@@ -1,28 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
 	return 'Hello World - this is the nlp module'
-
-
-@app.route('/classify', methods=['POST'])
-def classify():
-	data = request.get_json()
-
-	twee_text = data['text']
-	label, precision = predict(image)
-
-	response = {
-		"label": label,
-		"precision": precision
-	}
-
-	return json.dumps(response)
-
-
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", debug=True)
--- nlp-module/tokenizer_component/tokenizer.py
+++ nlp-module/tokenizer_component/tokenizer.py
@@ -1,30 +1,22 @@
-import string
-
 from .tokens_collection import TokensCollection
-from .custom_token import *
-from nltk.tokenize import word_tokenize
+from .tweet_text_state import TweetTextState
 
 
 class Tokenizer:
     _state = None
 
-    def __init__(self, tweet_text: str) -> None:
+    def __init__(self, state, tweet_text: str) -> None:
+        self._tokens_collection = TokensCollection()
         self._tweet_text = tweet_text
+        self.transition_to(state)
 
     def get_tokens_collection(self) -> TokensCollection:
-        tokens_list = word_tokenize(self._tweet_text)
-        for index, raw_token in enumerate(tokens_list):
-            if raw_token in string.punctuation:
-                token = Token(TokenType.punctuation_mark, raw_token)
-            elif len(raw_token.split(" ")) > 1:
-                token = Token(TokenType.composed, raw_token)
-            else:
-                token = Token(TokenType.simple, raw_token)
-            tokens_list[index] = token
-        return TokensCollection(tokens_list)
+        return self._tokens_collection
 
-    def delete_stop_words(self):
-        pass
+    def transition_to(self, state):
+        self._state = state
+        self._state.context = self
 
-    def lemmatizing(self):
-        pass
+    # Request based on state
+    def parse_tweet(self):
+        self._state.parse_tweet_handle(crt_tokens_collection=self._tokens_collection)
